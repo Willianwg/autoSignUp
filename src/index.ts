d847b5899e1227cdfcfcdcbfd115b6f745629b4c
Willian <willian.wg70@gmail.com>
2022-10-09 14:51:29 -0300

    add puppeteer and method to get a text in facebook login page

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import readline from 'readline-sync';
+import puppeteer from 'puppeteer';
 
 async function machine():Promise<void>{
     await askUser();
-
+    await Pup();
 }
 
 async function askUser(): Promise<void>{
@@ -10,6 +11,22 @@ async function askUser(): Promise<void>{
     console.log(userResponse);
 }
 
+async function Pup() {
+     const browser = await puppeteer.launch();
+     // Create a new incognito browser context.
+     const context = await browser.createIncognitoBrowserContext();
+     // Create a new page in a pristine context.
+     const page = await context.newPage();
+     // Do stuff
+     const url = 'https://pt-br.facebook.com/';
+     await page.goto(url);
+
+     const result = await page.evaluate(
+         () => document.querySelector('._8eso').textContent 
+     )
+
+    console.log(result);
+    browser.close();
+   };
+
 machine();
-
-
